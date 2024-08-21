b49bfd9126bf46dcb0015703add9e7ed321a9c97
MarcVergire1988 <vergeiremarcedison@gmail.com>
2024-08-21 01:45:07 -0700

    11th commit

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+]
+
+export default user;
